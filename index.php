8285cba570d31623412b2ef4fed1c65aaa918c01
ThomasMurat <thomasmurat1992@gmail.com>
2020-06-05 15:02:00 +0200

    up exo7

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Php-Partie1-exo7</title>
+    </head>
+    <body>
+    <?php 
+        $add = 'Thomas';
+        $multipliate = 'Murat';
+        $divide = 22;
+        $phrase = 'Bonjour ' . $lastname . ' ' . $firstname . ', tu as ' . $age . 'ans.';
+    ?>
+    <p><?php echo $phrase ;?></p>
+    </body>
+</html>
